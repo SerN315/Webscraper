52763fc9927e8f3fa7e713a312c44d2c56d7f186
SerNguyen <phamsynguyen2003@gmail.com>
2024-01-16 10:10:15 +0700

    sua format data inprogress

--- CNDPT.py
+++ CNDPT.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 def scrape():
     options = Options()
     options.add_argument('--headless')
-    driver = webdriver.Edge(options=options)
+    driver = webdriver.Chrome()
     try:
         # Truy cập vào trang web
         driver.get("https://www.lazada.vn/")
@@ -33,12 +33,12 @@ def scrape():
             link = title_link_element.get_attribute("href")
             driver.get(link)
             time.sleep(2)
+            title_element= driver.find_element(By.XPATH,"//h1[@class='pdp-mod-product-badge-title']")
             body = driver.find_element(By.TAG_NAME,'body')
             body.send_keys(Keys.PAGE_DOWN)
             body.send_keys(Keys.PAGE_DOWN)
             body.send_keys(Keys.PAGE_DOWN)
             time.sleep(20)
-            title_element= driver.find_element(By.XPATH,"//h1[@class='pdp-mod-product-badge-title']")
             summary_element = driver.find_element(By.CSS_SELECTOR,".mod-rating .content .left .summary")
             rating_score = summary_element.find_element(By.CLASS_NAME, "score")
             rating_score_average = rating_score.find_element(By.CLASS_NAME, "score-average")
@@ -48,26 +48,45 @@ def scrape():
                 star_url = star_image.get_attribute("src")
                 star_urls.append(star_url)
             rating_count = summary_element.find_element(By.CSS_SELECTOR, ".count")
-            comments = driver.find_elements(By.CSS_SELECTOR, ".mod-reviews .item")
-            comment_contents = []  # List to store comment_content for all items
-            for comment in comments:
-                star_score_per = comment.find_element(By.CSS_SELECTOR, ".top .container-star")
-                star_images_per = star_score_per.find_elements(By.TAG_NAME, "img")
-                star_urls_per = []
-                for star_image_per in star_images_per:
-                    star_url_per = star_image_per.get_attribute("src")
-                    star_urls_per.append(star_url_per)
-                d_name_spans = comment.find_elements(By.CSS_SELECTOR, ".middle span")
-                comment_content = comment.find_element(By.CSS_SELECTOR, ".item-content .content")
-                comment_text = comment_content.text
-                comment_contents.append(comment_text)  # Append comment_text to the list
+            filter_button = driver.find_element(By.CSS_SELECTOR, ".pdp-mod-filterSort .oper[data-spm-anchor-id='a2o4n.pdp_revamp.ratings_reviews.i0.604a41cacAcTDZ']")
+            filter_button.click()
+            time.sleep(20)
+            li_elements = driver.find_element(By.CSS_SELECTOR,".next-menu-content li")
+            column_mapping = {
+                0:"All-Stars",
+                1:"5 Stars",
+                2:"4 Stars",
+                3:"3 Stars",
+                4:"2 Stars",
+                5:"1 Stars",
+            }
+            for index in range(1,len(li_elements)):
+                li_element = li_elements[index]
+                if li_element.get_attribute("class") != "next-menu-item disabled":
+        # Click on the <li> element
+                   li_element.click()
+                   time.sleep(2)  # Wait for the comments to update
+                comments = driver.find_elements(By.CSS_SELECTOR, ".mod-reviews .item")
+                comment_contents = []  # List to store comment_content for all items
+                for comment in comments:
+                    star_score_per = comment.find_element(By.CSS_SELECTOR, ".top .container-star")
+                    star_images_per = star_score_per.find_elements(By.TAG_NAME, "img")
+                    star_urls_per = []
+                    for star_image_per in star_images_per:
+                        star_url_per = star_image_per.get_attribute("src")
+                        star_urls_per.append(star_url_per)
+                    d_name_spans = comment.find_elements(By.CSS_SELECTOR, ".middle span")
+                    comment_content = comment.find_element(By.CSS_SELECTOR, ".item-content .content")
+                    comment_text = comment_content.text
+                    comment_contents.append(comment_text)  # Append comment_text to the list
 
 
             # Lấy nội dung của các phần tử khác
             title = title_element.text
             score = rating_score_average.text
             numbs = rating_count.text 
-            # username = d_name_spans[0].text
+            username = d_name_spans[0].text
+            column_value = column_mapping[index]
             # verify = d_name_spans[2].text
 
             row_data={
@@ -75,9 +94,9 @@ def scrape():
                 "DG":score,
                 "DG_average_image":star_urls,
                 "SoDG":numbs,
-                # "TenHienthi":username,
-                # "verify":verify,
-                "comment_DG":star_urls_per,
+                "TenHienthi":username,
+                "DG_rieng":column_value,
+                # "comment_DG":star_urls_per,
                 "comment":comment_contents
 
             }
@@ -91,9 +110,9 @@ def scrape():
         write_headers = not os.path.exists("lazada.csv")
 
         # Ghi dữ liệu vào file CSV
-        with open("lazada.csv", "a", newline="", encoding="utf-8") as csv_file:
+        with open("lazada.csv", "w", newline="", encoding="utf-8") as csv_file:
             fieldnames = [
-                    "TenSP", "DG", "DG_average_image", "SoDG","comment_DG","comment"
+                    "TenSP", "DG", "SoDG","username","DG_rieng","comment"
                 ]
             writer = DictWriter(csv_file, fieldnames=fieldnames)
             write_headers = True
